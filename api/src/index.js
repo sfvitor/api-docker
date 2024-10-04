c13999cca99064418971263e96cccc09877a551a
Vitor Fernandes <vitorinfocefet@gmail.com>
2024-10-04 00:43:05 -0300

    adds database logic

--- api/src/index.js
+++ api/src/index.js
@@ -1,4 +1,17 @@
 const cors = require('cors');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite',
+});
+
+class Admin extends Model {}
+Admin.init({
+    name: DataTypes.STRING,
+}, { sequelize, modelName: 'admin' });
+
+sequelize.sync();
 
 const app = require('express')();
 
@@ -6,6 +19,21 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello Docker v0.2!' });
+});
+
+app.get('/admin', async (req, res) => {
+    const [admin] = await Admin.findAll();
+    res.json({ data: admin?.name });
+});
+
+app.post('/admin/:name', async (req, res) => {
+    const { name } = req.params;
+    const [admin] = await Admin.findAll();
+    (!admin
+        ? await Admin.create({ name })
+        : await admin.update({ name })
+    );
+    res.json({ data: name });
 });
 
 const port = process.env.PORT || 8888;
--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,19 +1,27 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 function App() {
   const [title, setTitle] = useState('');
+  const [admin, setAdmin] = useState('');
 
   useEffect(() => {
     (async function() {
-      const result = await fetch(import.meta.env.VITE_API_URL)
+      const titleResult = await fetch(apiUrl)
         .then(res => res.json());
-      setTitle(result.message);
+      setTitle(titleResult.message);
+
+      const adminResult = await fetch(`${apiUrl}/admin`)
+        .then(res => res.json());
+      setAdmin(adminResult.data);
     })();
   }, []);
 
   return (
     <>
       <h1>{title}</h1>
+      {admin && <h2>Admin is {admin}</h2>}
     </>
   )
 }
