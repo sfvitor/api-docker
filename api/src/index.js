ac81041f6f4ab2afff3ad77c9eb014006eb53536
Vitor Fernandes <vitorinfocefet@gmail.com>
2024-10-07 09:57:23 -0300

    test for api volume

--- api/src/index.js
+++ api/src/index.js
@@ -3,7 +3,7 @@ const { Sequelize, Model, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: '../db/database.sqlite',
+    storage: './database.sqlite', // ../db/database.sqlite
 });
 
 class Admin extends Model {}
